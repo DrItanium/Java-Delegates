2fcbb5893542a2f87e6b0a018ed0d0374ce2a6f2
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-04-02 12:25:54 -0700

    Fixed an issue in DynamicDelegateBase with a comment
    I had accidentally commented out a curly brace and screwed up the
    DynamicDelegateBase class.

--- functions/DynamicDelegateBase.java
+++ functions/DynamicDelegateBase.java
@@ -27,83 +27,84 @@ package functions;
 
 public abstract class DynamicDelegateBase extends Hashtable<String, Object> implements DynamicDelegate {
 
-	public DynamicDelegateBase() {
-		initVariables();
-	}
+   public DynamicDelegateBase() {
+      initVariables();
+   }
 
-	public DynamicDelegateBase(DynamicDelegate b) {
-		this();
-		Set<String> keys = b.names();
-		for (String str : keys) {
-			Object var = b.getDynamicVariable(str);
-			if (var == null) //null check...just in case {
-				put(str, null);
-			}
-			if (var instanceof NonLocalClosedVariable) {
-				NonLocalClosedVariable v = new NonLocalClosedVariable((NonLocalClosedVariable) var);
-				put(str, v);
-			} else if (var instanceof ClosedVariable) {
-				ClosedVariable v = new ClosedVariable((ClosedVariable) var);
-				put(str, v);
-			} else {
-				put(str, var);
-			}
-		}
-	}
+   public DynamicDelegateBase(DynamicDelegate b) {
+      this();
+      Set<String> keys = b.names();
+      for (String str : keys) {
+         Object var = b.getDynamicVariable(str);
+         //null check just in case
+         if (var == null) {
+            put(str, null);
+         }
+         if (var instanceof NonLocalClosedVariable) {
+            NonLocalClosedVariable v = new NonLocalClosedVariable((NonLocalClosedVariable) var);
+            put(str, v);
+         } else if (var instanceof ClosedVariable) {
+            ClosedVariable v = new ClosedVariable((ClosedVariable) var);
+            put(str, v);
+         } else {
+            put(str, var);
+         }
+      }
+   }
 
-	protected abstract void initVariables();
+   protected abstract void initVariables();
 
-	public boolean registerDynamicVariable(String name, Object value, boolean readonly) {
-		if (dynamicVariableExists(name)) {
-			return false;
-		}
-		Object var = readonly ? new ClosedVariable(value) : new NonLocalClosedVariable(value);
-		put(name, var);
-		return true;
-	}
+   public boolean registerDynamicVariable(String name, Object value, boolean readonly) {
+      if (dynamicVariableExists(name)) {
+         return false;
+      }
+      Object var = readonly ? new ClosedVariable(value) : new NonLocalClosedVariable(value);
+      put(name, var);
+      return true;
+   }
 
-	public boolean dynamicVariableExists(String name) {
-		return containsKey(name);
-	}
+   public boolean dynamicVariableExists(String name) {
+      return containsKey(name);
+   }
 
-	private boolean isReadonly(String name) {
-		try {
-			Object value = getDynamicVariable(name);
-			return !(value instanceof NonLocalClosedVariable);
-		} catch (NoSuchElementException n) {
-			return false;
-		}
-	}
+   private boolean isReadonly(String name) {
+      try {
+         Object value = getDynamicVariable(name);
+         return !(value instanceof NonLocalClosedVariable);
+      } catch (NoSuchElementException n) {
+         return false;
+      }
+   }
 
-	public void setDynamicVariable(String name, Object value) throws DynamicVariableReadonlyException {
-		if (!isReadonly(name)) {
-			NonLocalClosedVariable oldValue = (NonLocalClosedVariable) getDynamicVariable(name);
-			oldValue.actualValue(value);
-		} else {
-			throw new DynamicVariableReadonlyException();
-		}
-	}
+   public void setDynamicVariable(String name, Object value) throws DynamicVariableReadonlyException {
+      if (!isReadonly(name)) {
+         NonLocalClosedVariable oldValue = (NonLocalClosedVariable) getDynamicVariable(name);
+         oldValue.actualValue(value);
+      } else {
+         throw new DynamicVariableReadonlyException();
+      }
+   }
 
-	public Object getDynamicVariable(String name) {
-		if (!containsKey(name)) {
-			throw new NoSuchElementException("The provided key " + name + " does not exist.");
-		} else {
-			return get(name);
-		}
-	}
+   public Object getDynamicVariable(String name) {
+      if (!containsKey(name)) {
+         throw new NoSuchElementException("The provided key " + name + " does not exist.");
+      } else {
+         return get(name);
+      }
+   }
 
-	public <T> T getDynamicVariableAsType(String name) {
-		if (!containsKey(name)) {
-			throw new NoSuchElementException("The provided key " + name + " does not exist.");
-		} else {
-			return (T)as(get(name));
-		}
-	}
+   public <T> T getDynamicVariableAsType(String name) {
+      if (!containsKey(name)) {
+         throw new NoSuchElementException("The provided key " + name + " does not exist.");
+      } else {
+         return (T)as(get(name));
+      }
+   }
 
-	public abstract DynamicDelegate copy();
-	public abstract Object clone();
+   public abstract DynamicDelegate copy();
+   public abstract Object clone();
 
-	public Set<String> getNames() {
-		return super.keySet();
-	}
+   public Set<String> getNames() {
+      return super.keySet();
+   }
 }
