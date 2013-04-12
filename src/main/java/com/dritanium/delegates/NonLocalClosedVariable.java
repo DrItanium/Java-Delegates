cda01965101c67253ca7c8255d8e7ff077679d48
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-04-12 14:24:09 -0700

    Fixed an incorrect package name for NonLocalClosedVariable.java
    I had put com.dritanium.functions instead of com.dritanium.delegates.

--- src/main/java/com/dritanium/delegates/NonLocalClosedVariable.java
+++ src/main/java/com/dritanium/delegates/NonLocalClosedVariable.java
@@ -23,7 +23,7 @@
 //The views and conclusions contained in the software and documentation are those of the
 //authors and should not be interpreted as representing official policies, either expressed
 //or implied, of Joshua Scoggins. 
-package com.dritanium.functions;
+package com.dritanium.delegates;
 public class NonLocalClosedVariable<T> extends ClosedVariable<ClosedVariable<T>> {
 	public T getActualValue() {
 		return getValue().getValue();
