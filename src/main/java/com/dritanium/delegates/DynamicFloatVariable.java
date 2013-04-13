20c4435c6a6b3d256edb9e6b2488d8b2a2738e15
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-04-12 23:18:36 -0700

    Commented the DynamicFloatVariable class

--- src/main/java/com/dritanium/delegates/DynamicFloatVariable.java
+++ src/main/java/com/dritanium/delegates/DynamicFloatVariable.java
@@ -26,8 +26,8 @@
 package com.dritanium.delegates;
 
 /**
- *
- * @author jwscoggins
+ * A dynamic variable that stores a float
+ * @author Joshua Scoggins
  */
 public class DynamicFloatVariable extends DynamicNumberVariable<Float>{
 	public DynamicFloatVariable(String name, Float value, boolean isReadonly) {
